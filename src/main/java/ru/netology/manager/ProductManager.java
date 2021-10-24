5810f7cb8c5fb3853e95531401ade586667bc9b3
Valeriya <tmcxezsim5nn@mail.ru>
2021-10-24 15:48:47 +0300

    Initial commit

---
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -0,0 +1,57 @@
+package ru.netology.manager;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.TShirt;
+import ru.netology.repository.ProductRepository;
+
+@AllArgsConstructor
+@Data
+public class ProductManager {
+    private ProductRepository items;
+
+
+    public void add(Product item) {
+        items.save(item);
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product item : items.getAll()) {
+            if (matches(item, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = item;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product item, String search) {
+        if (item instanceof Book) {
+            Book book = (Book) item;
+            if (book.getAuthor().contains(search)) {
+                return true;
+            }
+            if (book.getName().contains(search)) {
+                return true;
+            }
+            return false;
+        }
+        if (item instanceof TShirt) {
+            TShirt smartphone = (TShirt) item;
+            if (smartphone.getManufacturer().contains(search)) {
+                return true;
+            }
+            if (smartphone.getName().contains(search)) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ru/netology/repository/RepositoryTest.java
@@ -0,0 +1,49 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.NotFoundException;
+import ru.netology.domain.Product;
+import ru.netology.domain.TShirt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepositoryTest {
+
+    ProductRepository repo = new ProductRepository();
+
+    private Book book1 = new Book(1, "Jane Eyre", 600, "Bronte");
+    private Book book2 = new Book(2, "Three Men in a Boat", 700, "Jerome");
+    private Book book3 = new Book(3, "Iphone", 1_000, "Poug");
+    private TShirt tShirt1 = new TShirt(4, "TShirt1", 20_000, "Nike");
+    private TShirt tShirt2 = new TShirt(5, "TShirt2", 15_000, "Demix");
+    private TShirt tShirt3 = new TShirt(6, "TShirt3", 17_000, "Cropp");
+
+    @BeforeEach
+    public void setUp() {
+        
+        repo.save(book1);
+        repo.save(book2);
+        repo.save(book3);
+        repo.save(tShirt1);
+        repo.save(tShirt2);
+        repo.save(tShirt3);
+    }
+
+    @Test
+    public void shouldRemoveByExistElement() {
+        Product[] actual = repo.removeById(1);
+        Product[] expected = new Product[] {book2, book3, tShirt1, tShirt2, tShirt3};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveByNotExistElement() {
+
+        assertThrows(NotFoundException.class, () -> {
+            repo.removeById(100);
+        });
+    }
+}
